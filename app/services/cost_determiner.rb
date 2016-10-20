514762a67ae00c01836bbbbb37f1d19e61cea17d
David Salgado <david.salgado@digital.justice.gov.uk>
2016-10-20 12:11:37 +0100

    Add stubs for appeal cost and cost determinator

---
+++ app/services/cost_determiner.rb
@@ -0,0 +1,33 @@
+# Given an appeal object, figure out the cost to the appellant of lodging the appeal,
+# and any likely further costs (such as determination fee)
+class CostDeterminer
+  attr_reader :appeal
+
+  def initialize(appeal)
+    @appeal = appeal
+  end
+
+  def run
+    case appeal.appeal_about
+    when :income_tax
+      income_tax_appeal_cost
+    when :apn_penalty, :closure_notice, :information_notice, :request_permission_for_review, :other
+      AppealCost.new(lodgement_fee: 50.0)
+    else
+      raise "Unable to determine cost of appeal"
+    end
+  end
+
+  private
+
+  def income_tax_appeal_cost
+    case appeal.dispute_about
+    when :paye_coding_notice
+      AppealCost.new(lodgement_fee: 50.0)
+    when :amount_of_tax_owed
+      AppealCost.new(lodgement_fee: 200.0)
+    else
+      raise "Unable to determine cost of income tax appeal"
+    end
+  end
+end
---
+++ spec/services/cost_determiner_spec.rb
@@ -0,0 +1,88 @@
+require 'rails_helper'
+
+RSpec.describe CostDeterminer do
+  let(:appeal_attrs) { {} }
+  let(:appeal) { double(appeal_attrs) }
+
+  subject { described_class.new(appeal) }
+
+  # TODO: Move this context to the appeal model specs, when we have some
+  context "when hmrc challenged is false" do
+    let(:appeal_attrs) { super().merge(hmrc_challenged: false) }
+
+    # TODO: Enable these specs
+    context "when appeal is about income tax" do
+      let(:appeal_attrs) { super().merge(appeal_about: :income_tax) }
+
+      xspecify { expect(appeal).not_to be_valid }
+
+      xit "says you must challenge hmrc" do
+        expect(appeal.errors.challenge_hmrc).to be_truthy
+      end
+    end
+  end
+
+  context "when hmrc challenged is true" do
+    let(:appeal_attrs) { super().merge(hmrc_challenged: true) }
+
+    context "when appeal is about advance payment notice penalty" do
+      let(:appeal_attrs) { super().merge(appeal_about: :apn_penalty) }
+
+      it "has £50 lodgement fee" do
+        expect(subject.run.lodgement_fee).to eq(50.0)
+      end
+    end
+
+    context "when appeal is about a closure notice" do
+      let(:appeal_attrs) { super().merge(appeal_about: :closure_notice) }
+
+      it "has £50 lodgement fee" do
+        expect(subject.run.lodgement_fee).to eq(50.0)
+      end
+    end
+
+    context "when appeal is about an information notice" do
+      let(:appeal_attrs) { super().merge(appeal_about: :information_notice) }
+
+      it "has £50 lodgement fee" do
+        expect(subject.run.lodgement_fee).to eq(50.0)
+      end
+    end
+
+    context "when appeal is about request for review" do
+      let(:appeal_attrs) { super().merge(appeal_about: :request_permission_for_review) }
+
+      it "has £50 lodgement fee" do
+        expect(subject.run.lodgement_fee).to eq(50.0)
+      end
+    end
+
+    context "when appeal is about something else" do
+      let(:appeal_attrs) { super().merge(appeal_about: :other) }
+
+      it "has £50 lodgement fee" do
+        expect(subject.run.lodgement_fee).to eq(50.0)
+      end
+    end
+
+    context "when appeal is about income tax" do
+      let(:appeal_attrs) { super().merge(appeal_about: :income_tax) }
+
+      context "when dispute is about amount of tax owed" do
+        let(:appeal_attrs) { super().merge(dispute_about: :amount_of_tax_owed) }
+
+        it "has £200 lodgement fee" do
+          expect(subject.run.lodgement_fee).to eq(200.0)
+        end
+      end
+
+      context "when dispute is about paye coding" do
+        let(:appeal_attrs) { super().merge(dispute_about: :paye_coding_notice) }
+
+        it "has £50 lodgement fee" do
+          expect(subject.run.lodgement_fee).to eq(50.0)
+        end
+      end
+    end
+  end
+end
