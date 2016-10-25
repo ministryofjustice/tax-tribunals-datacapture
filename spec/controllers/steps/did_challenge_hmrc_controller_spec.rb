4e2490ed198d1a0281ff0a5563e5a0e8c1132b48
Christian Sutter <chris@doublesignal.com>
2016-10-25 14:53:53 +0100

    Minor improvement to controller spec from PR #12 (#13)

--- spec/controllers/steps/did_challenge_hmrc_controller_spec.rb
+++ spec/controllers/steps/did_challenge_hmrc_controller_spec.rb
@@ -3,23 +3,38 @@ require 'rails_helper'
 RSpec.describe Steps::DidChallengeHmrcController, type: :controller do
   describe 'GET /edit' do
     context 'when no case exists in the session yet' do
-      it 'creates a case and shows the question to the user' do
-        get :edit
+      it 'creates a new case' do
+        expect { get :edit }.to change { TribunalCase.count }.by(1)
+      end
 
-        expect(response).to have_http_status(:success)
-        expect(TribunalCase.count).to be(1)
+      it 'responds with HTTP success' do
+        get :edit
+        expect(response).to be_successful
+      end
+
+      it 'sets the case ID in the session' do
+        get :edit
         expect(session[:tribunal_case_id]).to eq(TribunalCase.first.id)
       end
     end
 
     context 'when a case exists in the session' do
-      it 'uses the existing case and shows the question to the user' do
-        existing_case = TribunalCase.create
-        get :edit, session: { tribunal_case_id: existing_case.id }
+      let!(:existing_case) { TribunalCase.create }
 
-        expect(response).to have_http_status(:success)
-        expect(TribunalCase.count).to be(1)
-        expect(session[:tribunal_case_id]).to eq(TribunalCase.first.id)
+      it 'does not create a new case' do
+        expect {
+          get :edit, session: { tribunal_case_id: existing_case.id }
+        }.to_not change { TribunalCase.count }
+      end
+
+      it 'responds with HTTP success' do
+        get :edit, session: { tribunal_case_id: existing_case.id }
+        expect(response).to be_successful
+      end
+
+      it 'does not change the case ID in the session' do
+        get :edit, session: { tribunal_case_id: existing_case.id }
+        expect(session[:tribunal_case_id]).to eq(existing_case.id)
       end
     end
   end
