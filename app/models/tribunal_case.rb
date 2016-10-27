6484eca93357ad486b0633dd677c7c958b3458dd
Christian Sutter <chris@doublesignal.com>
2016-10-27 15:05:41 +0100

    Tweak TribunalCase values to satisfy mutant

--- app/models/tribunal_case.rb
+++ app/models/tribunal_case.rb
@@ -1,14 +1,16 @@
 class TribunalCase < ApplicationRecord
+  WHAT_IS_APPEAL_ABOUT_VALUES = %w(
+    income_tax
+    vat
+    apn_penalty
+    inaccurate_return
+    closure_notice
+    information_notice
+    request_permission_for_review
+    other
+  )
+
   def self.what_is_appeal_about_values
-    %w(
-      income_tax
-      vat
-      apn_penalty
-      inaccurate_return
-      closure_notice
-      information_notice
-      request_permission_for_review
-      other
-    )
+    WHAT_IS_APPEAL_ABOUT_VALUES
   end
 end
