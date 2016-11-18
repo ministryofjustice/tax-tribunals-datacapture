503b7080a495768872015c1cd5818077780362d2
Christian Sutter <chris@doublesignal.com>
2016-11-18 11:46:21 +0000

    Update GOV.UK Elements & FE Toolkit (#44)
    This gives us the new radio buttons and the latest updates
    of GDS magic.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+//= require govuk_toolkit
+
+$(document).ready(function() {
+  // Use GOV.UK selection-buttons.js to set selected
+  // and focused states for block labels
+  var $blockLabels = $(".block-label input[type='radio'], .block-label input[type='checkbox']");
+  new GOVUK.SelectionButtons($blockLabels);
+});
