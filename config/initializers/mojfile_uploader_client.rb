271bf1537a5bf78c1bf28a506e2efb1e9942c459
Jesus Laiz <zheileman@users.noreply.github.com>
2016-12-08 17:34:00 +0000

    Adding the MOJ_FILE_UPLOADER to the .env files, and not using default.

--- config/initializers/mojfile_uploader_client.rb
+++ config/initializers/mojfile_uploader_client.rb
@@ -1,6 +1,6 @@
 require 'mojfile_uploader_api_client'
 
 MojFileUploaderApiClient::HttpClient.configure do |client|
-  client.base_url = ENV.fetch('MOJ_FILE_UPLOADER', 'http://localhost:9292')
+  client.base_url = ENV.fetch('MOJ_FILE_UPLOADER')
   # client.options = {}
 end
