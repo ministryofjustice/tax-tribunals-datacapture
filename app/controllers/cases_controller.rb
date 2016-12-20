e15928dc96f0a9308081f67787a7ffa20f1ef492
Jesus Laiz <zheileman@users.noreply.github.com>
2016-12-20 09:37:33 +0000

    Remove TODO.

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -5,7 +5,6 @@ class CasesController < ApplicationController
     result_url = if new_case.success?
                    new_case.payment_url
                  else
-                   # TODO: change once we have the summary step
                    flash[:alert] = new_case.errors
                    steps_details_check_answers_path
                  end
