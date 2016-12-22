5125aa5b9c8db804e5b75ef508ded20241375bbe
Christian Sutter <chris@doublesignal.com>
2016-12-22 10:53:14 +0000

    Update the step generator to the state of the art (#82)
    - Assume that a form will have a value object rather than assuming
      that it hasn't (and give guidance in the comments on what to do if
      it hasn't)
    - Update controller to reflect change in `update_and_advance` method
      signature
    - Update documentation

--- lib/generators/step/templates/form.rb
+++ lib/generators/step/templates/form.rb
@@ -3,42 +3,31 @@ module Steps::<%= task_name.camelize %>
     # TODO: Add more attributes or change type if necessary
     attribute :<%= step_name.underscore %>, String
 
-    # TODO: Choices
-    #   - Uncomment the below if you have a value object
-    #   - Delete the method if you haven't
-    #
-    # def self.choices
-    #  <%= step_name.camelize %>.values.map(&:to_s)
-    # end
-    # validates_inclusion_of :<%= step_name.underscore %>, in: choices
-
-    # TODO: Add any further validations here
+    # TODO: Delete this method and add different validation if you don't have a value object
+    def self.choices
+      <%= step_name.camelize %>.values.map(&:to_s)
+    end
+    validates_inclusion_of :<%= step_name.underscore %>, in: choices
 
     private
 
-    # TODO:
-    #   - Uncomment the below if you have a value object
-    #   - Delete the method if you haven't
-    # def <%= step_name.underscore %>_value
-    #  <%= step_name.camelize %>.new(<%= step_name.underscore %>)
-    # end
+    # TODO: Delete this method if you don't have a value object
+    def <%= step_name.underscore %>_value
+      <%= step_name.camelize %>.new(<%= step_name.underscore %>)
+    end
 
+    # TODO: Change this method if you don't have a single value object
     def changed?
-      # TODO: Make this return whether the form data has changed from the current
-      #  state of the tribunal_case object. If you have a value object, you could
-      #  do e.g.:
-      #
-      #  tribunal_case.<%= step_name.underscore %> != <%= step_name.underscore %>_value
-      raise 'TODO: Update <%= step_name.camelize %>#changed?'
+      tribunal_case.<%= step_name.underscore %> != <%= step_name.underscore %>_value
     end
 
     def persist!
       raise 'No TribunalCase given' unless tribunal_case
       return true unless changed?
 
-      # TODO: Update this to persist your form object
+      # TODO: Update this to persist your form object if you don't have a single value object
       tribunal_case.update(
-        <%= step_name.underscore %>: (raise 'TODO: Update <%= step_name.camelize %>#persist!')
+        <%= step_name.underscore %>: <%= step_name.underscore %>_value
         # The following are dependent attributes that need to be reset
         # TODO: Are there any dependent attributes? Reset them here.
       )
