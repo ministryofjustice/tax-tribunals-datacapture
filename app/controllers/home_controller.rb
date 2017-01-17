2d4322d0ea8bd99e96e2881d4863ed8481c0d0b0
Jesus Laiz <zheileman@users.noreply.github.com>
2017-01-17 11:19:48 +0000

    Link to payment app.
    A new env variable PAYMENT_EXTERNAL_URL has been exposed to store the URL
    to the fees payment app page where the user will land when clicking in the
    `pay for an existing appeal` link in the datacapture Home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
+    @link_sections = link_sections
+  end
+
+  private
+
+  # [task name, estimated time to complete this task, path to the task]
+  def link_sections
+    [
+      [:appeal, 30, task_list_path],
+      [:close, 15, '#'],
+      [:pay, 5, ENV.fetch('PAYMENT_EXTERNAL_URL')]
+    ]
   end
 end
---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe HomeController do
+  describe '#index' do
+    before do
+      expect(ENV).to receive(:fetch).with('PAYMENT_EXTERNAL_URL').and_return('http://payments.url')
+    end
+
+    it 'assigns the expected link sections' do
+      get :index
+
+      name, time, link = assigns[:link_sections][0]
+      expect(name).to eq(:appeal)
+      expect(time).to eq(30)
+      expect(link).to eq('/task_list')
+
+      name, time, link = assigns[:link_sections][1]
+      expect(name).to eq(:close)
+      expect(time).to eq(15)
+      expect(link).to eq('#')
+
+      name, time, link = assigns[:link_sections][2]
+      expect(name).to eq(:pay)
+      expect(time).to eq(5)
+      expect(link).to eq('http://payments.url')
+    end
+  end
+end
