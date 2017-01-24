d1abeec8ea4ba8f206deb37bb5436354e7bb7acd
Todd Tyree <tatyree@gmail.com>
2017-01-24 16:18:28 +0000

    Don’t test the external libraries (facepalm)

--- spec/services/contact_detail_validator_spec.rb
+++ spec/services/contact_detail_validator_spec.rb
@@ -1,137 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe ContactDetailValidator do
+  describe '#contact_detail' do
+    subject { described_class.new(' ABC123 ') }
+
+    it 'strips leading and trailing whitespace from input' do
+      expect(subject.contact_detail).to eq('ABC123')
+    end
+  end
+
   describe '#valid_postcode?' do
-    subject { described_class.valid_postcode?(postcode) }
+    let(:postcode) { 'W1A 2AB' }
+    let(:validator) { double }
 
-    context 'a valid postcode' do
-      let(:postcode) { 'W1A 2AB' }
-      specify { is_expected.to be_truthy }
-    end
-
-    context 'a valid postcode (not case sensitive)' do
-      let(:postcode) { 'w1A 2aB' }
-      specify { is_expected.to be_truthy }
-    end
-
-    context 'a valid postcode (without internal spaces)' do
-      let(:postcode) { 'W1A2AB' }
-      specify { is_expected.to be_truthy }
-    end
-
-    context 'a valid postcode with leading spaces' do
-      let(:postcode) { ' W1A 2AB' }
-      specify { is_expected.to be_truthy }
-    end
-
-    context 'a valid postcode with trailing spaces' do
-      let(:postcode) { 'W1A 2AB ' }
-      specify { is_expected.to be_truthy }
-    end
-
-    context 'a mangled postcode' do
-      let(:postcode) { 'W1A 2 A B' }
-      specify { is_expected.to be_falsey}
-    end
-
-    context 'partial postcode' do
-      let(:postcode) { 'W1A' }
-      specify { is_expected.to be_falsey }
-    end
-
-    context 'something that looks like a postcode' do
-      let(:postcode) { 'X1Z 1ZZ' }
-      specify { is_expected.to be_falsey }
-    end
-
-    context 'something other than a postcode' do
-      let(:postcode) { '118 118' }
-      specify { is_expected.to be_falsey }
+    it 'processes the postcode with an external validator' do
+      expect(validator).to receive(:full_valid?)
+      expect(UKPostcode).to receive(:parse).with(postcode).and_return(validator)
+      described_class.valid_postcode?(postcode)
     end
   end
 
   describe '#valid_email?' do
-    subject { described_class.valid_email?(email) }
+    let(:email) { 'test@example.com' }
 
-    context "a 'typical' email" do
-      let(:email) { 'test@example.com' }
-      specify { is_expected.to be_truthy }
-    end
-
-    context "a 'typical' email with leading spaces" do
-      let(:email) { ' test@example.com' }
-      specify { is_expected.to be_truthy }
-    end
-
-    context "a 'typical' email with trailing spaces" do
-      let(:email) { 'test@example.com ' }
-      specify { is_expected.to be_truthy }
-    end
-
-    context "a mangled 'typical' email" do
-      let(:email) { 'test@examp le .com ' }
-      specify { is_expected.to be_falsey}
-    end
-
-    context 'a bad email' do
-      let(:email) { 'x@y.z' }
-      specify { is_expected.to be_falsey }
-    end
-
-    context 'something other than an email' do
-      let(:email) { 'W1A 1AA' }
-      specify { is_expected.to be_falsey }
+    it 'processes email with an external validator' do
+      expect(EmailValidator).to receive(:valid?).with(email)
+      described_class.valid_email?(email)
     end
   end
 
   describe '#valid_phone?' do
-    subject { described_class.valid_phone?(phone) }
+    let(:phone) { '02033343555' }
 
-    # 'Valid' in the sense of a valid format. Other examples use the official
-    # MoJ landline number.
-    context 'a valid mobile' do
-      let(:phone) { '07766554433' }
-      specify { is_expected.to be_truthy }
-    end
-
-    context 'a valid landline' do
-      let(:phone) { '02033343555' }
-      specify { is_expected.to be_truthy }
-    end
-
-    context 'a valid landline with internal spacing' do
-      let(:phone) { '020 3334 3555' }
-      specify { is_expected.to be_truthy }
-    end
-
-    context 'a valid phone with a country code' do
-      let(:phone) { '+44 20 3334 3555' }
-      specify { is_expected.to be_truthy }
-    end
-
-    context 'a valid phone with leading spaces' do
-      let(:phone) { ' 02033343555' }
-      specify { is_expected.to be_truthy }
-    end
-
-    context 'a valid phone with trailing spaces' do
-      let(:phone) { '02033343555 ' }
-      specify { is_expected.to be_truthy }
-    end
-
-    context 'a valid phone with trailing spaces' do
-      let(:phone) { '02033343555 ' }
-      specify { is_expected.to be_truthy }
-    end
-
-    context 'an invalid phone' do
-      let(:phone) { '12033343555 ' }
-      specify { is_expected.to be_falsey }
-    end
-
-    context 'something that is not a phone at all' do
-      let(:phone) { 'test@example.com' }
-      specify { is_expected.to be_falsey }
+    it 'processes phone number with an external validator' do
+      expect(Phonelib).to receive(:valid_for_country?).with(phone, :gb)
+      described_class.valid_phone?(phone)
     end
   end
 end
