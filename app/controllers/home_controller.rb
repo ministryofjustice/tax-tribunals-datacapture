5a30e4b231f9cea93b2156b067101f82f646f47c
Todd Tyree <tatyree@gmail.com>
2017-01-27 12:14:34 +0000

    Remove "pay for existing appeal" from start page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,6 @@ class HomeController < ApplicationController
     [
       [:appeal, 30, task_list_path],
       [:close, 15, steps_closure_start_path],
-      [:pay, 5, ENV.fetch('FEES_EXTERNAL_URL')]
     ]
   end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,10 +2,6 @@ require 'rails_helper'
 
 RSpec.describe HomeController do
   describe '#index' do
-    before do
-      expect(ENV).to receive(:fetch).with('FEES_EXTERNAL_URL').and_return('http://payments.url')
-    end
-
     it 'assigns the expected link sections' do
       get :index
 
@@ -18,11 +14,6 @@ RSpec.describe HomeController do
       expect(name).to eq(:close)
       expect(time).to eq(15)
       expect(link).to eq('/steps/closure/start')
-
-      name, time, link = assigns[:link_sections][2]
-      expect(name).to eq(:pay)
-      expect(time).to eq(5)
-      expect(link).to eq('http://payments.url')
     end
   end
 end
