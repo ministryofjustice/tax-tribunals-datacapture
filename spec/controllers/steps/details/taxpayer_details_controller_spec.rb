e1a9199c4039dc48e17ad0408d57e7eca5bc03e1
Christian Sutter <chris@doublesignal.com>
2017-01-31 10:57:00 +0000

    Consolidate taxpayer details flow (#133)
    In preparation for six different taxpayer/representative screens (one
    for each combination of {taxpayer,rep} and {individual,company,
    organisation}), simplify and DRY up the controller, view and form logic.
    
    - Consolidate controllers into a single controller for all taxpayer
      types
    - Consolidate views into one and add logic to form objects to determine
      which fields to show
    - Update decision tree to consolidate into one step for taxpayer details

---
+++ spec/controllers/steps/details/taxpayer_details_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Steps::Details::TaxpayerDetailsController, type: :controller do
+  before do
+    allow(controller).to receive(:current_tribunal_case).and_return(tribunal_case)
+  end
+
+  context 'given an individual taxpayer' do
+    let(:tribunal_case) { TribunalCase.new(taxpayer_type: ContactableEntityType::INDIVIDUAL) }
+
+    it_behaves_like 'an intermediate step controller', Steps::Details::TaxpayerIndividualDetailsForm, DetailsDecisionTree
+  end
+
+  context 'given a company taxpayer' do
+    let(:tribunal_case) { TribunalCase.new(taxpayer_type: ContactableEntityType::COMPANY) }
+
+    it_behaves_like 'an intermediate step controller', Steps::Details::TaxpayerOrganisationDetailsForm, DetailsDecisionTree
+  end
+
+  context 'given an organisation taxpayer' do
+    let(:tribunal_case) { TribunalCase.new(taxpayer_type: ContactableEntityType::OTHER_ORGANISATION) }
+
+    it_behaves_like 'an intermediate step controller', Steps::Details::TaxpayerOrganisationDetailsForm, DetailsDecisionTree
+  end
+end
---
+++ spec/forms/steps/details/taxpayer_individual_details_form_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+RSpec.describe Steps::Details::TaxpayerIndividualDetailsForm do
+  it_behaves_like 'a contactable entity form',
+    entity_type: :taxpayer,
+    additional_fields: [
+      :taxpayer_individual_name
+    ]
+
+  describe '#name_fields' do
+    specify { expect(subject.name_fields).to eq([:taxpayer_individual_name]) }
+  end
+
+  describe '#show_fao?' do
+    specify { expect(subject.show_fao?).to eq(false) }
+  end
+
+  describe '#show_registration_number?' do
+    specify { expect(subject.show_registration_number?).to eq(false) }
+  end
+end
