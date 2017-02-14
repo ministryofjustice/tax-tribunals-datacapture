6102174a4b96ec91da8ff6d17c544b97e5cc12e1
Jesus Laiz <zheileman@users.noreply.github.com>
2017-02-14 10:32:13 +0000

    Same pattern for first step as in appeal flow.

--- app/controllers/steps/closure/case_type_controller.rb
+++ app/controllers/steps/closure/case_type_controller.rb
@@ -1,5 +1,5 @@
 module Steps::Closure
-  class CaseTypeController < Steps::ClosureStepController
+  class CaseTypeController < Steps::ClosureFirstStepController
     def edit
       super
       @form_object = CaseTypeForm.new(
@@ -10,23 +10,6 @@ module Steps::Closure
 
     def update
       update_and_advance(CaseTypeForm, as: :case_type)
-    end
-
-    private
-
-    def current_tribunal_case
-      # This step, and only this step, should create a tribunal case if
-      # there isn't one in the session - because it's the first
-      # TODO: Reconsider where this should go - it's not very intuitive
-      #   to be doing this here.
-      super || initialize_tribunal_case(intent: Intent::CLOSE_ENQUIRY)
-    end
-
-    def update_navigation_stack
-      # This is the first step in the closure flow, so reset the navigation stack
-      # before re-initialising it in StepController#update_navigation_stack
-      current_tribunal_case.navigation_stack = []
-      super
     end
   end
 end
