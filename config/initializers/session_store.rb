1413e030050ea4d31da921e2bc5c4d12ee6f182f
Jesus Laiz <zheileman@users.noreply.github.com>
2017-02-22 09:47:26 +0000

    Some better copy for session expired error page.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 Rails.application.config.session_store :cookie_store,
   key:          '_tax-tribunals-datacapture_session',
-  expire_after: 30.minutes
+  expire_after: Rails.configuration.session_expire_after.minutes
