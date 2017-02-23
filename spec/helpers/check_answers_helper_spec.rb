492aca4d5e104b51df143b4eb75927f77d8c3385
Jesus Laiz <zheileman@users.noreply.github.com>
2017-02-23 10:32:58 +0000

    Fix case PDF translation to use correct appeal/application naming.
    As we are using a custom t() for the PDF (so it has cascading), we forgot to add
    the capability to know when to use appeal or application.

--- spec/helpers/check_answers_helper_spec.rb
+++ spec/helpers/check_answers_helper_spec.rb
@@ -2,14 +2,18 @@ require 'rails_helper'
 
 RSpec.describe CheckAnswersHelper do
   describe '#pdf_t' do
-    # These are just 2 keys to perform a quick sanity check of the method.
+    # These are just a few keys to perform a quick sanity check of the method.
     # If ever changed or removed, any other keys can be used instead.
     let(:found_key) { '.questions.penalty_level' }
     let(:cascaded_key) { '.questions.lateness_reason' }
+    let(:interpolation_key) { '.questions.case_type' }
 
     before do
       # Without setting this virtual path, we cannot mimic the lazy lookup of the view
       helper.instance_variable_set(:@virtual_path, 'steps.details.check_answers.pdf.show')
+      expect_any_instance_of(ApplicationHelper).to receive(:appeal_or_application_params).and_return(
+        {appeal_or_application: 'headache'}
+      )
     end
 
     it 'returns a found key' do
@@ -20,6 +24,11 @@ RSpec.describe CheckAnswersHelper do
     it 'cascades a not found key' do
       result = helper.pdf_t(cascaded_key)
       expect(result).to eq('Reason for lateness?')
+    end
+
+    it 'interpolates the appeal_or_application params' do
+      result = helper.pdf_t(interpolation_key)
+      expect(result).to eq('What is your headache about?')
     end
   end
 end
