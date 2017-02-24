e8d2353dd7e6b8086ce666cb26ec4a313045f692
Jesus Laiz <zheileman@users.noreply.github.com>
2017-02-24 16:18:13 +0000

    Some refactor and improvements.

--- app/mailers/notify_mailer.rb
+++ app/mailers/notify_mailer.rb
@@ -1,10 +1,11 @@
 class NotifyMailer < GovukNotifyRails::Mailer
-  #
+  CASE_SUBMITTED_TEMPLATE_ID = 'e64e9db9-d344-4041-a7df-135fbd39fa56'.freeze
+
   # Define methods as usual, and set the template and personalisation, if needed,
   # then just use mail() as with any other ActionMailer, with the recipient email
   #
   def case_submitted(tribunal_case)
-    set_template('e64e9db9-d344-4041-a7df-135fbd39fa56')
+    set_template(CASE_SUBMITTED_TEMPLATE_ID)
 
     set_personalisation(
       first_name: tribunal_case.taxpayer_individual_first_name,
