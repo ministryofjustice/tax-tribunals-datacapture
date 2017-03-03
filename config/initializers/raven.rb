d208def9644ba102af90bb191a8c548df23ad86a
Jesus Laiz <zheileman@users.noreply.github.com>
2017-03-03 12:24:57 +0000

    Requiring Raven and getting rid of the test fakes. (#234)

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,6 +1,5 @@
 # :nocov:
 if %w( production ).include?(Rails.env) && ENV['SENTRY_DSN'].present?
-  require 'raven'
   Raven.configure do |config|
     config.dsn = ENV['SENTRY_DSN']
     config.environments = %w( production )
