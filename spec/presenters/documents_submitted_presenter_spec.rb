ef89fe00e10b8f80fa2aa2ed2483a8d8905cc3b0
Jesus Laiz <zheileman@users.noreply.github.com>
2017-03-10 12:23:13 +0000

    'Check your answers' has Hardship answers and document (if any attached).
    Ensure we show all the relevant information for Hardship, in a similar way to previous PR
    for grounds for appeal.
    
    As part of this PR a bit of refactoring and simplification was done.
    
    https://www.pivotaltracker.com/story/show/141398683

--- spec/presenters/documents_submitted_presenter_spec.rb
+++ spec/presenters/documents_submitted_presenter_spec.rb
@@ -3,35 +3,41 @@ require 'spec_helper'
 RSpec.describe DocumentsSubmittedPresenter do
   subject { described_class.new(tribunal_case) }
 
-  let(:tribunal_case) { instance_double(TribunalCase, grounds_for_appeal: grounds_for_appeal) }
-  let(:grounds_for_appeal) { 'foo' }
-
   describe '#list' do
-    it 'should retrieve any uploaded file for the given documents prefix' do
-      expect(tribunal_case).to receive(:documents).with(:prefix)
-      subject.list(:prefix)
+    let(:tribunal_case) { instance_double(TribunalCase, having_problems_uploading_documents?: having_problems) }
+
+    context 'when no problems uploading documents' do
+      let(:having_problems) { false }
+
+      it 'should retrieve any uploaded file for the given documents prefix' do
+        expect(tribunal_case).to receive(:documents).with(:prefix)
+        subject.list(:prefix)
+      end
+    end
+
+    context 'when user was having problems uploading documents' do
+      let(:having_problems) { true }
+
+      it 'should return an empty collection' do
+        expect(tribunal_case).not_to receive(:documents).with(:prefix)
+        expect(subject.list(:prefix)).to eq([])
+      end
     end
   end
 
-  describe '#grounds_for_appeal_text' do
-    context 'when grounds_for_appeal text provided' do
-      it 'should return the grounds_for_appeal text' do
-        expect(subject.grounds_for_appeal_text).to eq('foo')
-      end
+  describe '#text_value_for' do
+    context 'when text is present for the given document prefix' do
+      let(:tribunal_case) { instance_double(TribunalCase, grounds_for_appeal: 'foo', hardship_reason: 'bar') }
+
+      it { expect(subject.text_value_for(:grounds_for_appeal)).to eq('foo') }
+      it { expect(subject.text_value_for(:hardship_reason)).to eq('bar') }
     end
 
-    context 'when grounds_for_appeal text is not present' do
-      let(:grounds_for_appeal) { nil }
+    context 'when text is not present for the given document prefix' do
+      let(:tribunal_case) { instance_double(TribunalCase, grounds_for_appeal: nil, hardship_reason: nil) }
 
-      it 'should return a default text if provided' do
-        text = subject.grounds_for_appeal_text(default: 'default text')
-        expect(text).to eq('default text')
-      end
-
-      it 'should return nil if no default text is provided' do
-        text = subject.grounds_for_appeal_text
-        expect(text).to be_nil
-      end
+      it { expect(subject.text_value_for(:grounds_for_appeal)).to be_nil }
+      it { expect(subject.text_value_for(:hardship_reason)).to be_nil }
     end
   end
 end
