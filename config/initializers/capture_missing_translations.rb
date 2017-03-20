d5f2d894076e5ac09145ca56f682d8fd719000e8
Jesus Laiz <zheileman@users.noreply.github.com>
2017-03-20 16:52:34 +0000

    Silently send to Sentry missing translation, without user error.
    Capture any missing translation exceptions without showing them to the user, send them to Sentry
    and let the `translate` or `t` helpers return a sensible fallback to the user, without the app
    blowing app.
    
    On dev and test environment we continue raising exceptions, but not anymore on prod.

---
+++ config/initializers/capture_missing_translations.rb
@@ -0,0 +1,22 @@
+# We want to be alerted via Sentry about any missing translation happening on production, without
+# having to raise an exception, blowing up the app and showing an error message to the user.
+#
+# Overriding the `MissingTranslationData` class to capture the exception whenever initialized, we make
+# sure we are alerted each time this exception is generated, without having to enable the config flag
+# `raise_on_missing_translations` on production environment, and thus letting the `translate` or `t`
+# helpers return a sensible fallback translation to the user.
+#
+module I18n
+  class MissingTranslationData
+    def initialize(locale, key, options = nil)
+      super
+
+      Raven.extra_context(
+        locale: locale,
+        scope: options[:scope],
+        key: key
+      )
+      Raven.capture_exception(self)
+    end
+  end
+end
