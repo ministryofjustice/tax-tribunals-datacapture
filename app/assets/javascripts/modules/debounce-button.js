58404ad3d9f06877ee9d2ca993ad918af4f37cef
Clive Murray <hi+github@clivemurray.com>
2017-03-23 15:34:53 +0000

    js debounce for case submit button (#306)

---
+++ app/assets/javascripts/modules/debounce-button.js
@@ -0,0 +1,28 @@
+'use strict';
+
+moj.Modules.debounceButton = {
+  button_class: '.js-debounce',
+
+  init: function() {
+    var self = this;
+
+    if($(self.button_class).length) {
+      self.bindEvents();
+    }
+  },
+
+  bindEvents: function() {
+    var self = this,
+        disableClass = 'disabled';
+
+    $(self.button_class).on('click', function(e) {
+      var $button = $(e.target);
+
+      if($button.hasClass(disableClass)) {
+        e.preventDefault();
+      } else {
+        $button.addClass(disableClass);
+      }
+    });
+  }
+};
