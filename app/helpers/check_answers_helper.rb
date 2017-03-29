b8b62c77176b929aca8ca480d37fec04c9b1c466
Christian Sutter <chris@doublesignal.com>
2017-03-29 15:18:50 +0100

    Remove PDF-specific check answers translations (#330)
    We originally added the ability to override specific questions/answers
    in the "Check your answers" PDF to make them different from the copy
    shown on the HTML version. After conversations with design and content,
    this is no longer required - so this removes both the translation and
    the underlying code from the app.
    
    - Remove `UploadHelper` and `UploadHelper#pdf_t`
    - Use regular `I18n.translate`/`t` instead of `pdf_t` in PDF view
      templates
    - Remove remaining PDF-specific translations

--- app/helpers/check_answers_helper.rb
+++
@@ -1,9 +0,0 @@
-module CheckAnswersHelper
-  # Tries _pdf suffix for translations to allow overriding of I18n in PDF
-  def pdf_t(key, options = {})
-    fallback = translate_with_appeal_or_application(key, options)
-    options_with_fallback = options.merge(default: fallback)
-
-    translate_with_appeal_or_application("#{key}_pdf", options_with_fallback)
-  end
-end
