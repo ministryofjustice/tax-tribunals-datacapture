6dafad6b1f729319af55af60dcfecd4be36769e1
Jesus Laiz <zheileman@users.noreply.github.com>
2017-04-12 16:47:24 +0100

    Removing obsolote prototype html pages and routes.
    Now we have the `real thing`©

--- config/routes.rb
+++ config/routes.rb
@@ -113,8 +113,6 @@ Rails.application.routes.draw do
   get :contact, to: 'home#contact', as: :contact_page
   get :terms_and_conditions, to: 'home#terms_and_conditions'
   get :appeal_saved, to: 'home#appeal_saved'
-  get :sign_in, to: 'home#sign_in'
-  get :saved_appeals, to: 'home#saved_appeals'
   get :example_saved_appeal, to: 'home#example_saved_appeal'
 
   # catch-all route
