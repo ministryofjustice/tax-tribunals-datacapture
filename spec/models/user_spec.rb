beae4172ba14c9a3071ae75ce9e887fb61ac92b1
Todd Tyree <tatyree@gmail.com>
2017-04-26 14:17:27 +0100

    Ensure sign in time is available and recorded

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+  describe '#last_sign_in_at' do
+    specify { expect(described_class.column_names).to include('last_sign_in_at') }
+  end
 end
