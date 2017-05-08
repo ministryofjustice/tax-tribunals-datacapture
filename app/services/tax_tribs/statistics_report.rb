90bcac486584b5ece674a85d560e17083f39b4b5
David Salgado <david.salgado@digital.justice.gov.uk>
2017-05-08 12:44:03 +0100

    Add TaxTribs::StatisticsReport service class

---
+++ app/services/tax_tribs/statistics_report.rb
@@ -0,0 +1,32 @@
+class TaxTribs::StatisticsReport
+  def self.cases_by_date_csv
+    results = TribunalCase.connection.execute <<~SQL
+      SELECT
+        date(created_at) AS created_on,
+        case_status,
+        count(*) AS number
+      FROM #{TribunalCase.table_name}
+      GROUP BY date(created_at), case_status
+    SQL
+
+    dates = results.inject({}) do |hash, row|
+      d = row.fetch('created_on')
+
+      data = hash.fetch(d, { 'started' => 0, 'submitted' => 0 })
+
+      started = data.fetch('started')
+      started += row.fetch('number')
+
+      submitted = data.fetch('submitted')
+      submitted += row.fetch('number') if row.fetch('case_status').eql?(CaseStatus::SUBMITTED.to_s)
+
+      hash[d] = { 'started' => started, 'submitted' => submitted }
+
+      hash
+    end
+
+    dates.inject(["Date, Started, Submitted"]) do |arr, (date, data)|
+      arr.push [date, data.fetch('started'), data.fetch('submitted')].join(', ')
+    end.join("\n")
+  end
+end
