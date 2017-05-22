b7c7090ff0d98cf454a9870b39c32db4868c28fd
David Salgado <david.salgado@digital.justice.gov.uk>
2017-05-22 10:46:55 +0100

    Count started and submitted cases separately
    We need two different SQL queries to get cases started by
    (created) date, and cases submitted by (updated) date.

--- spec/services/tax_tribs/statistics_report_spec.rb
+++ spec/services/tax_tribs/statistics_report_spec.rb
@@ -4,6 +4,29 @@ RSpec.describe TaxTribs::StatisticsReport do
   describe '#cases_by_date_csv' do
     let(:header) { "Date, Started, Submitted" }
 
+    context 'when case is submitted days after creation' do
+      before do
+        tribunal_case = nil
+
+        travel_to(Time.parse('2017-05-08 06:30:00')) do
+          tribunal_case = TribunalCase.create({ case_status: CaseStatus.new(:first_reminder_sent) })
+        end
+
+        travel_to(Time.parse('2017-05-10 06:30:00')) do
+          tribunal_case.update(case_status: CaseStatus.new(:submitted))
+        end
+      end
+
+      it 'reports started and submitted on separate dates' do
+        report = [
+          header,
+          '2017-05-08, 1, 0',
+          '2017-05-10, 0, 1'
+        ].join("\n")
+        expect(described_class.cases_by_date_csv).to eq(report)
+      end
+    end
+
     context 'when there are no TribunalCase records' do
       specify { expect(described_class.cases_by_date_csv).to eq(header) }
     end
@@ -25,16 +48,15 @@ RSpec.describe TaxTribs::StatisticsReport do
         end
       end
 
-      it "generates a csv report" do
+      it 'generates a csv report' do
         report = [
           header,
-          "1970-01-01, 3, 1",
-          "2017-04-08, 1, 0",
-          "2017-05-08, 1, 0"
+          '1970-01-01, 3, 1',
+          '2017-04-08, 1, 0',
+          '2017-05-08, 1, 0'
         ].join("\n")
         expect(described_class.cases_by_date_csv).to eq(report)
       end
-
     end
   end
 end
