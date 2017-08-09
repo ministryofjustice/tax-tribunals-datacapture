05a1ef212c06caa8e0f1f81adb92453c339ac112
Jesus Laiz <zheileman@users.noreply.github.com>
2017-08-09 12:33:10 +0100

    Adapt check your answers to cope with new steps
    Some minor changes to the copy we show depending on the letter uploader
    the user went through.

--- app/presenters/check_answers/details_section_presenter.rb
+++ app/presenters/check_answers/details_section_presenter.rb
@@ -9,9 +9,24 @@ module CheckAnswers
         FileOrTextAnswer.new(:grounds_for_appeal, tribunal_case.grounds_for_appeal, tribunal_case.documents(:grounds_for_appeal).first, change_path: edit_steps_details_grounds_for_appeal_path),
         # nil file because there is no file upload field (yet)
         FileOrTextAnswer.new(:outcome, tribunal_case.outcome, nil, change_path: edit_steps_details_outcome_path),
-        DocumentsSubmittedAnswer.new(:letter_submitted, tribunal_case.documents(:supporting_letter), change_path: edit_steps_details_letter_upload_path),
-        Answer.new(:problems_uploading_letter, tribunal_case.having_problems_uploading_explanation, raw: true, change_path: edit_steps_details_letter_upload_path)
+        *submitted_letter_files_answers
       ].select(&:show?)
+    end
+
+    private
+
+    def submitted_letter_files_answers
+      if tribunal_case.letter_upload_type == LetterUploadType::MULTIPLE
+        [
+          DocumentsSubmittedAnswer.new(:letter_files_uploaded, tribunal_case.documents(:supporting_documents), change_path: edit_steps_details_documents_upload_path),
+          Answer.new(:problems_uploading_letter, tribunal_case.having_problems_uploading_explanation, raw: true, change_path: edit_steps_details_documents_upload_path)
+        ]
+      else
+        [
+          DocumentsSubmittedAnswer.new(:letter_uploaded, tribunal_case.documents(:supporting_letter), change_path: edit_steps_details_letter_upload_path),
+          Answer.new(:problems_uploading_letter, tribunal_case.having_problems_uploading_explanation, raw: true, change_path: edit_steps_details_letter_upload_path)
+        ]
+      end
     end
   end
 end
--- spec/presenters/check_answers/details_section_presenter_spec.rb
+++ spec/presenters/check_answers/details_section_presenter_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 module CheckAnswers
   describe DetailsSectionPresenter do
     subject { described_class.new(tribunal_case) }
-    let(:tribunal_case) { TribunalCase.new }
+    let(:tribunal_case) { TribunalCase.new(letter_upload_type: letter_upload_type) }
+    let(:letter_upload_type) { nil }
 
     let(:documents_submitted_answer) { instance_double(DocumentsSubmittedAnswer, show?: true) }
     let(:file_or_text_answer) { instance_double(FileOrTextAnswer, show?: true) }
@@ -11,8 +12,6 @@ module CheckAnswers
 
     before do
       allow(tribunal_case).to receive(:documents).and_return([])
-
-      allow(DocumentsSubmittedAnswer).to receive(:new).and_return(documents_submitted_answer)
       allow(FileOrTextAnswer).to receive(:new).and_return(file_or_text_answer)
       allow(Answer).to receive(:new).and_return(answer)
     end
@@ -24,13 +23,40 @@ module CheckAnswers
     end
 
     describe '#answers' do
-      it 'has the correct rows' do
-        expect(subject.answers.count).to eq(4)
+      context 'for a single letter file upload' do
+        before do
+          allow(DocumentsSubmittedAnswer).to receive(:new).with(
+            :letter_uploaded, [], change_path: '/steps/details/letter_upload'
+          ).and_return(documents_submitted_answer)
+        end
 
-        expect(subject.answers[0]).to eq(file_or_text_answer)
-        expect(subject.answers[1]).to eq(file_or_text_answer)
-        expect(subject.answers[2]).to eq(documents_submitted_answer)
-        expect(subject.answers[3]).to eq(answer)
+        it 'has the correct rows' do
+          expect(subject.answers.count).to eq(4)
+
+          expect(subject.answers[0]).to eq(file_or_text_answer)
+          expect(subject.answers[1]).to eq(file_or_text_answer)
+          expect(subject.answers[2]).to eq(documents_submitted_answer)
+          expect(subject.answers[3]).to eq(answer)
+        end
+      end
+
+      context 'for a multi letter files upload' do
+        let(:letter_upload_type) { LetterUploadType::MULTIPLE }
+
+        before do
+          allow(DocumentsSubmittedAnswer).to receive(:new).with(
+            :letter_files_uploaded, [], change_path: '/steps/details/documents_upload'
+          ).and_return(documents_submitted_answer)
+        end
+
+        it 'has the correct rows' do
+          expect(subject.answers.count).to eq(4)
+
+          expect(subject.answers[0]).to eq(file_or_text_answer)
+          expect(subject.answers[1]).to eq(file_or_text_answer)
+          expect(subject.answers[2]).to eq(documents_submitted_answer)
+          expect(subject.answers[3]).to eq(answer)
+        end
       end
     end
   end
