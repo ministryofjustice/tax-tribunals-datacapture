4a8dfa46dc872c795bc7592ed0c2791eb428aad7
Jesus Laiz <zheileman@users.noreply.github.com>
2017-10-10 12:06:49 +0100

    Copy changes and removing duplication in CYA (#588)
    Small copy change and removing a duplication when both the penalty level
    and the amount were present.
    Now only the amount will show in the check your answers page.

--- app/presenters/check_answers/appeal_section_presenter.rb
+++ app/presenters/check_answers/appeal_section_presenter.rb
@@ -10,8 +10,7 @@ module CheckAnswers
         challenged_decision_answer,
         Answer.new(:challenged_decision_status, tribunal_case.challenged_decision_status, change_path: edit_steps_challenge_decision_status_path),
         dispute_type_answer,
-        Answer.new(:penalty_level, tribunal_case.penalty_level, change_path: edit_steps_appeal_penalty_amount_path),
-        Answer.new(:penalty_amount, tribunal_case.penalty_amount, raw: true),
+        penalty_level_or_amount_answer,
         Answer.new(:tax_amount, tribunal_case.tax_amount, raw: true)
       ].select(&:show?)
     end
@@ -31,6 +30,14 @@ module CheckAnswers
         Answer.new(:dispute_type, tribunal_case.dispute_type_other_value, raw: true, change_path: edit_steps_appeal_dispute_type_path)
       else
         Answer.new(:dispute_type, tribunal_case.dispute_type, change_path: edit_steps_appeal_dispute_type_path)
+      end
+    end
+
+    def penalty_level_or_amount_answer
+      if tribunal_case.penalty_amount.blank?
+        Answer.new(:penalty_level, tribunal_case.penalty_level)
+      else
+        Answer.new(:penalty_amount, tribunal_case.penalty_amount, raw: true)
       end
     end
 
