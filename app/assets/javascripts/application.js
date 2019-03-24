e01d2fd1962a643592dff085db8e03c6d2ef8717
Leonidas Apostolidis <leoapost@gmail.com>
2019-03-24 12:23:04 +0000

    Upgrade govuk gems

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -146,11 +146,6 @@ function ShowHideContent() {
 }
 
 $(document).ready(function() {
-  // Use GOV.UK selection-buttons.js to set selected
-  // and focused states for block labels
-  var $blockLabels = $(".block-label input[type='radio'], .block-label input[type='checkbox']");
-  new GOVUK.SelectionButtons($blockLabels);
-
   // Show and hide toggled content
   // Where .block-label uses the data-target attribute
   var toggleContent = new ShowHideContent();
