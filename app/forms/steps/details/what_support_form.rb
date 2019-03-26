0e725bf5f72568bfa289ec25ea4579d8c19bab34
Leonidas Apostolidis <leoapost@gmail.com>
2019-03-26 14:13:14 +0000

    Fix validation UI issue in the what support page

--- app/forms/steps/details/what_support_form.rb
+++ app/forms/steps/details/what_support_form.rb
@@ -9,7 +9,7 @@ module Steps::Details
     attribute :other_support_details, String
 
     validates_presence_of  :other_support_details, if: :other_support?
-    validate :at_least_one_selected
+    validate :at_least_one_checkbox_validation
 
     private
 
@@ -27,16 +27,16 @@ module Steps::Details
       attribute_set.map { |attr| [attr.name, self[attr.name]] }.to_h
     end
 
-    def at_least_one_selected
-      errors.add(:what_support, "Select what support you need at the hearing") unless any_answer?
+    def at_least_one_checkbox_validation
+      errors.add(:what_support, "Select what support you need at the hearing") unless any_answers?
     end
 
-    def any_answer?
+    def any_answers?
       language_interpreter ||
       sign_language_interpreter ||
       hearing_loop ||
       disabled_access ||
-      other_support.present?
+      other_support
     end
   end
 end
