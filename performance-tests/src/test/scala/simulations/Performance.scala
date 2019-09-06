4da846c3b22063c7bccc351b42bd73fcdd8dd2d1
Alexa <alexa.ballantine@gmail.com>
2019-09-06 11:51:32 +0100

    WIP removed login details and hwf test

--- performance-tests/src/test/scala/simulations/Performance.scala
+++ performance-tests/src/test/scala/simulations/Performance.scala
@@ -7,94 +7,33 @@ import com.typesafe.config._
 
 
 
-class StaffAppPerformance extends Simulation
+class Performance extends Simulation
 with HttpConfiguration
 {
   val conf = ConfigFactory.load()
   val baseurl = conf.getString("baseUrl")
   val httpconf = httpProtocol.baseURL(baseurl).disableCaching
 
-  val scenario1 = scenario("Happy path for Help with Fees staff app")
+  val scenario1 = scenario("Happy path for appeal against a tax decision")
 
     .exec(http("Store authenticity token")
-        .get("/users/sign_in")
+        .get("/appeal")
         .check(css("input[name='authenticity_token']", "value").saveAs("csrfCookie")))
 
-    .exec(http("Sign in")
-        .put("/users/sign_in")
-        .formParam("user[email]", conf.getString("userEmail"))
-        .formParam("user[password]", conf.getString("userPassword"))
+    .exec(http("Appeal")
+        .get("/users/sign_in")
         .formParam("authenticity_token", session => {
               session("csrfCookie").as[String]
         })
         .check(status.is(200)))
 
-    .exec(http("Paper application - Personal details")
-        .get("/applications/733032/personal_informations")
-        .formParam("application[first_name]", "Testy")  
-        .formParam("application[last_name]", "McTest-Face")
-        .formParam("application[day_date_of_birth]", "01")
-        .formParam("application[month_date_of_birth]", "02")
-        .formParam("application[year_date_of_birth]", "1974")
-        .formParam("application[married]", "false")
+    .exec(http("Case type")
+        .get("/steps/appeal/case_type")
+        .formParam("steps_appeal_case_type_form[case_type]", "income_tax")
         .formParam("authenticity_token", session => {
               session("csrfCookie").as[String]
         })
         .check(status.is(200)))
-
-    .exec(http("Paper application - Application details")
-        .get("/applications/733032/details")
-        .formParam("application[fee]", "500")
-        .formParam("application[jurisdiction_id]", "1")
-        .formParam("application[day_date_received]", "01")
-        .formParam("application[month_date_received]", "06")
-        .formParam("application[year_date_received]", "2019")
-        .formParam("application[form_name]", "C100")
-        .formParam("authenticity_token", session => {
-              session("csrfCookie").as[String]
-        })
-        .check(status.is(200)))
-
-    .exec(http("Paper application - Savings and investments")
-        .get("/applications/733032/savings_investments")
-        .formParam("application[min_threshold_exceeded]", "false")
-        .formParam("authenticity_token", session => {
-              session("csrfCookie").as[String]
-        })
-        .check(status.is(200)))
-
-    .exec(http("Paper application - Benefits")
-        .get("/applications/7330321/benefits")
-        .formParam("application[benefits]", "false")
-        .formParam("authenticity_token", session => {
-              session("csrfCookie").as[String]
-        })
-        .check(status.is(200)))
-
-    .exec(http("Paper application - Income")
-        .get("/applications/7330321/incomes")
-        .formParam("application[dependents]", "false")
-        .formParam("application[income]", "500")
-        .formParam("authenticity_token", session => {
-              session("csrfCookie").as[String]
-        })
-        .check(status.is(200)))
-
-    .exec(http("Paper application - Summary")
-        .get("/applications/7330321/summary")
-        .formParam("authenticity_token", session => {
-              session("csrfCookie").as[String]
-        })
-        .check(status.is(200)))
-
-    .exec(http("Paper application - Confirmation")
-        .get("/applications/7330321/confirmation")
-        .formParam("authenticity_token", session => {
-              session("csrfCookie").as[String]
-        })
-        .check(status.is(200)))
-
-
 
   val userCount = conf.getInt("users")
   val durationInSeconds  = conf.getLong("duration")
