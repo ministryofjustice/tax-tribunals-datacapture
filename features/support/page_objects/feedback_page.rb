0dd627eabce2b9b1eb712bb45d59cd66d766d7eb
Alexa <alexa.ballantine@gmail.com>
2019-12-09 13:22:42 +0000

    Rst-2365 changed selector for multiple choice

--- features/support/page_objects/feedback_page.rb
+++ features/support/page_objects/feedback_page.rb
@@ -4,7 +4,7 @@ class FeedbackPage < BasePage
   section :content, '#content' do
     element :header, 'h1', text: 'Report a problem'
     element :note, 'p', 'Please note: we cannot offer legal advice.'
-    element :someone_else_option, 'label', text: 'I have difficulty using computers, so someone filled in the online Tax Tribunal application for me'
+    elements :multiple_choice, '.multiple-choice'
     element :submit_button, "input[value='Submit']"
     element :name_error, '.error-message', text: 'Please enter a valid name'
     element :email_error, '.error-message', text: 'Please enter a valid email address'
@@ -17,7 +17,7 @@ class FeedbackPage < BasePage
       p current_url
       fill_in 'Name', with: 'John Smith'
       fill_in 'Email', with: 'test@test.com'
-      feedback_page.content.someone_else_option.click
+      feedback_page.content.multiple_choice[0].click
       fill_in 'Describe what you would like to report', with: 'I need help with my appeal'
       feedback_page.content.submit_button.click
     end
