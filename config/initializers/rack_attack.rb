30e1be686f7b1fcc96b5d7304ef705acf99b2f7a
Leonidas Apostolidis <leoapost@gmail.com>
2020-03-13 13:08:03 +0000

    Add Rack::Attack filters for the contact form

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -10,4 +10,36 @@ class Rack::Attack
     end
   end
 
+  ### Block email addresses from submitting the Report a problem page ###
+
+  # This blocklist forbids users/bots from submitting the Report a Problem page
+  # by comparing the form email address against the RACK_ATTACK_BLOCKLIST_EMAILS
+  # env var.
+  #
+  # The RACK_ATTACK_BLOCKLIST needs to be a comma separated string
+  # with email address or domain names followed after the @ symbol.
+  # e.g. "john@example.com,@suspicious-domain.com"
+
+  # Read RACK_ATTACK_BLOCKLIST_EMAILS and filter out non email or domain names
+  spammers = ENV.fetch('RACK_ATTACK_BLOCKLIST_EMAILS', '').split(/,\s*/).select do
+    |email| email.match? /@[A-Z0-9.-]+\.[A-Z]{2,4}/i
+  end
+  spammer_regexp = Regexp.union(spammers)
+
+  blocklist("block emails") do |req|
+    req.path =~ /feedback/i &&
+    req.post? &&
+    req.params['surveys_feedback_form']['email'] =~ spammer_regexp
+  end
+
+  ### Prevent DOS Attack on the Report a Problem page ###
+
+  # Throttle POST requests to /feedback paths by IP address
+  #
+  # Key: "rack::attack:#{Time.now.to_i/:period}:feedback/ip:#{req.remote_ip}"
+  throttle('feedback/ip', limit: 3, period: 2.minute) do |req|
+    if req.path =~ /feedback/i && req.post?
+      req.ip
+    end
+  end
 end
