39ae2f6f4d123491e88c6c7eb769c5fe0774c671
Leonidas Apostolidis <leoapost@gmail.com>
2020-04-03 10:38:09 +0100

    Debug Rails cache

--- app/controllers/surveys/survey_base_controller.rb
+++ app/controllers/surveys/survey_base_controller.rb
@@ -9,7 +9,8 @@ module Surveys
     def create
       # See what's stored in the Rails cache to make sure
       # the real user IP is being throttled and not the load balancer
-      logger.info("[DEBUG] Rails cache: #{Rails.cache.instance_variable_get(:@data).keys.map{|key| {key: key, value: Rails.cache.fetch(key)}}}")
+
+      logger.info("[DEBUG] Rails cache json: #{Rails.cache.to_json}")
 
       @form_object = form_object_class.new(survey_params)
 
