9eb6f15f35561fe0ffd14f1b8537ad4782588b6c
Leonidas Apostolidis <leoapost@gmail.com>
2020-05-28 19:02:23 +0100

    Update rails config files

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ Bundler.require(*Rails.groups)
 
 module TaxTribunalsDatacapture
   class Application < Rails::Application
-    config.load_defaults 5.2
+    config.load_defaults 6.0
 
     ActionView::Base.default_form_builder = GovukElementsFormBuilder::FormBuilder
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -9,11 +9,12 @@ Rails.application.configure do
 
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    'Cache-Control' => 'public, max-age=3600'
+    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
   }
 
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+  config.cache_store = :null_store
 
   config.action_dispatch.show_exceptions = false
 
