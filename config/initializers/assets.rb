7c47c9b84611f993d550927035fcb8a46e64ed34
Leonidas Apostolidis <leoapost@gmail.com>
2020-09-30 09:34:22 +0100

    Remove the local ie8 css file
    Already included from the new design system.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,7 +3,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile << %r{fonts/[\w-]+\.(?:eot|svg|ttf|woff2?)$}
 Rails.application.config.assets.precompile << %r{images/[\w-]+\.(?:png|svg)$}
 Rails.application.config.assets.precompile += %w(
-  ie8.css
   apple-touch-icon.png
   apple-touch-icon-180x180.png
   apple-touch-icon-167x167.png
