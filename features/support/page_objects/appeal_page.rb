ae658cf05ad96a89ae1cdf0ba5806a5eb61e67ad
Daniel Bell <daniel.bell@solirius.com>
2021-01-07 09:35:16 +0000

    Feature test refactor

--- features/support/page_objects/appeal_page.rb
+++ features/support/page_objects/appeal_page.rb
@@ -2,6 +2,6 @@ class AppealPage < BasePage
   set_url '/appeal'
 
   section :content, '#main-content' do
-    element :header, 'h1', text: 'Get an impartial decision on your dispute'
+    element :header, 'h1', text: 'Your appeal will be decided by an independent tribunal'
   end
 end
