9bd410ce47455c55f8442ab1a051c997e20ede9d
Petr Zaparka <petr@zaparka.cz>
2021-01-12 13:01:40 +0000

    Replaceing old sentry gem with new supported one

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,15 @@
+# :nocov:
+Sentry.init do |config|
+  config.dsn = ENV['SENTRY_DSN']
+  config.breadcrumbs_logger = [:sentry_logger, :active_support_logger]
+  config.environment = 'production'
+
+  # To activate performance monitoring, set one of these options.
+  # We recommend adjusting the value in production:
+  config.traces_sample_rate = 0.9
+
+ # This line is from old sentry gem config. I checked and the data sent to sentry UI are filtered based on the
+ # Rails.application.config.filter_parameters.map(&:to_s) values
+ # config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+end
+# :nocov:
