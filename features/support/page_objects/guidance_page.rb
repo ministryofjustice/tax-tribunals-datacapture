82ed6cabf2a914ee0fef772ea00aeebe1f0b1308
danielbe11 <70265071+danielbe11@users.noreply.github.com>
2021-01-22 10:52:59 +0000

    RST-2872 Guidance notes page (#731)
    * Add guidance notes page
    
    * Indexing the accordion section IDs
    
    * Unit tests added for guidance notes page
    
    * Feature tests added for guidance notes page
    
    * Updating features README
    
    * Added Print this page link
    
    * Relocated html tag
    
    * Grammar changes
    
    * Move back button and change feedback link
    
    * Fix to back button page object element
    
    * Remove _blank target from authorisation form link
    
    Co-authored-by: Petr Zaparka <petr@zaparka.cz>

---
+++ features/support/page_objects/guidance_page.rb
@@ -0,0 +1,20 @@
+class GuidancePage < BasePage
+  set_url '/guidance'
+
+  section :content, '#main-content' do
+    element :header, 'h1', text: 'Guide to appealing a tax decision'
+    element :visible_first_question, '#accordion-1-heading-0', visible: true
+    element :visible_second_question, '#accordion-1-heading-1', visible: true
+    element :visible_first_answer, '#accordion-1-content-0', visible: true
+    element :visible_second_answer, '#accordion-1-content-1', visible: true
+    element :open_all, '#accordion-1 .govuk-accordion__open-all'
+  end
+
+  def click_a_question
+    content.visible_first_question.click
+  end
+
+  def click_open_all
+    content.open_all.click
+  end
+end
