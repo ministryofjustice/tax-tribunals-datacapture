a8c1643acb8037ebebb04ddd6d0e3d36fb152712
Jerome Riga <jriga@zemis.co.uk>
2021-02-17 10:58:11 +0000

    Add support for ES6 for uglifier (#751)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   config.assets.css_compressor = false
   config.sass.style = :compressed
   config.sass.line_comments = false
