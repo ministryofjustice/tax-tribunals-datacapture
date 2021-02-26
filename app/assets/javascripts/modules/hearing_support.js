d57ca294befc4bdd2a00914d9300296e5512badb
Jerome Riga <jriga@zemis.co.uk>
2021-02-26 16:01:50 +0000

    WIP: position okay
    fix double click issue

--- app/assets/javascripts/modules/hearing_support.js
+++ app/assets/javascripts/modules/hearing_support.js
@@ -15,18 +15,46 @@ moj.Modules.hearingSupport = {
     conf: {
         width: 'element',
         theme: 'govuk',
+        selectOnClose: true,
         language: {
             noResults: function() {
                 return $('.select2-no-results').text();
             }
-        },
-        // selectionAdapter: function() {
-        //     console.log('this is callled!!!!!!!!')
-        // }
+        }
     },
 
-    bindEvents: function() {
+    dropdownAdapter: async function() {
+            return new Promise(function(resolve, reject) {
+                $.fn.select2.amd.require(
+                    ['select2/utils',
+                     'select2/dropdown',
+                     'select2/dropdown/search',
+                     'select2/dropdown/attachBody'],
+                    function (Utils, Dropdown, Search, AttachBody) {
+                        function PositionDropdown() {}
+                        PositionDropdown.prototype._positionDropdown = function (decorated) {
+                            decorated.call(this);
+                            var a = this.$dropdownContainer.offset();
+                            this.$dropdownContainer.css({
+                                top: a.top - 18,
+                                left: a.left - 5
+                            });
+                        };
+
+                        var DropdownSearch = Utils.Decorate(Dropdown, Search);
+                        var AttachedDropdownSearch = Utils.Decorate(DropdownSearch, AttachBody);
+                        return resolve(Utils.Decorate(AttachedDropdownSearch, PositionDropdown));
+                    }
+                );
+            });
+    },
+
+    bindEvents: async function() {
         const self = this;
+        let conf = self.conf;
+
+        conf.dropdownAdapter = await self.dropdownAdapter();
+
         $(self.selectors.select).select2(self.conf);
     }
 }
