31a424bfc414c551a63d9b82b405f9d0997d837b
AhsanZX97 <ahsan97@hotmail.co.uk>
2021-05-28 09:49:13 +0100

    updated email for production tests

--- features/step_definitions/save_and_return_steps.rb
+++ features/step_definitions/save_and_return_steps.rb
@@ -3,7 +3,7 @@ Given("I create an account in appeal journey") do
   save_return_page.content.create_account_checkbox.click
   continue_or_save_continue
   expect(save_appeal_page.content).to have_appeal_header
-  save_appeal_page.content.email_input.set 'test@hmcts.net'
+  save_appeal_page.content.email_input.set 'test_tt@hmcts.net'
   save_appeal_page.content.password_input.set 'TaxTribun4!'
   save
 end
@@ -13,7 +13,7 @@ Given("I create an account in closure journey") do
   save_return_page.content.create_account_checkbox.click
   continue_or_save_continue
   expect(save_appeal_page.content).to have_closure_header
-  save_appeal_page.content.email_input.set 'test@hmcts.net'
+  save_appeal_page.content.email_input.set 'test_tt@hmcts.net'
   save_appeal_page.content.password_input.set 'TaxTribun4!'
   save
 end
