9fc44f1154e99a9fce7c1c1c9579874f98f68e1a
Jerome Riga <jriga@zemis.co.uk>
2021-06-15 14:21:25 +0100

    RST-3373 validate tax amount and penalty amount
    **JIRA**
    [RST-3373](https://tools.hmcts.net/jira/browse/RST-3373)
    
    **Description**
        No validation for Money input fields

--- app/forms/steps/appeal/penalty_and_tax_amounts_form.rb
+++ app/forms/steps/appeal/penalty_and_tax_amounts_form.rb
@@ -3,9 +3,18 @@ module Steps::Appeal
     attribute :penalty_amount, String
     attribute :tax_amount, String
 
-    validates_presence_of :penalty_amount, :tax_amount
+    validates_numericality_of :tax_amount, greater_than: 0, unless: :unknown_tax_amount_entered?
+    validates_numericality_of :penalty_amount, greater_than: 0, unless: :unknown_penalty_amount_entered?
 
     private
+
+    def unknown_tax_amount_entered?
+      tax_amount&.downcase == I18n.t('dictionary.unknown')
+    end
+
+    def unknown_penalty_amount_entered?
+      penalty_amount&.downcase == I18n.t('dictionary.unknown')
+    end
 
     def persist!
       raise 'No TribunalCase given' unless tribunal_case
