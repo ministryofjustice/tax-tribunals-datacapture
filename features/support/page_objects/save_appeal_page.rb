9f51c7d0c46a5a1e22c5d196878db79f1899ca21
Will Taylor <wrftaylor@gmail.com>
2022-01-17 11:32:04 +0000

    Fix tests

--- features/support/page_objects/save_appeal_page.rb
+++ features/support/page_objects/save_appeal_page.rb
@@ -2,8 +2,8 @@ class SaveAppealPage < BasePage
   set_url '/'+ ENV['TEST_LOCALE'] +'/users/sign_up'
 
   section :content, '#main-content' do
-    element :appeal_header, 'h1', text: I18n.t('users.registrations.new.appeal_test')
-    element :closure_header, 'h1', text: I18n.t('users.registrations.new.application_test')
+    element :appeal_header, 'h1', text: I18n.t('users.registrations.new.heading_test_fixed_as_appeal')
+    element :closure_header, 'h1', text: I18n.t('users.registrations.new.heading_test_fixed_as_application')
     elements :login_label, '.govuk-label'
     element :email_input, '#user-email-field'
     element :password_input, '#user-password-field'
