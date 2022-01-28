93ba301dfd3193d67d41525e064a3564f701d987
Will <wrftaylor@gmail.com>
2022-01-28 11:30:19 +0000

    DEV 28 Jan - Show ips (#833)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -69,6 +69,10 @@ class ApplicationController < ActionController::Base
   end
 
   def show_maintenance_page(config = Rails.application.config)
+    if config.maintenance_enabled
+      Rails.logger.level = :debug
+      Rails.logger.debug("Remote IP: #{request.remote_ip}")
+    end
     return if !config.maintenance_enabled || config.maintenance_allowed_ips.include?(request.remote_ip)
 
     render 'static_pages/maintenance', status: :service_unavailable
