041d60bb62c1923cdf6bff37573aa4a48886b701
Will Taylor <wrftaylor@gmail.com>
2022-02-02 14:41:07 +0000

    Fix rubocop errors

--- app/controllers/steps/closure/eu_exit_controller.rb
+++ app/controllers/steps/closure/eu_exit_controller.rb
@@ -2,8 +2,8 @@ module Steps::Closure
   class EuExitController < Steps::ClosureStepController
     def edit
       @form_object = Steps::Shared::EuExitForm.new(
-          tribunal_case: current_tribunal_case,
-          eu_exit: current_tribunal_case.eu_exit
+        tribunal_case: current_tribunal_case,
+        eu_exit: current_tribunal_case.eu_exit
       )
       render template: 'steps/shared/eu_exit/edit'
     end
