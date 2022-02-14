037df30b3fa5ccfe61349962cfdee2cb8a315485
Will Taylor <wrftaylor@gmail.com>
2022-02-14 11:10:50 +0000

    Add EU exit page to PDF

--- features/support/page_objects/appeal/eu_exit_page.rb
+++ features/support/page_objects/appeal/eu_exit_page.rb
@@ -5,7 +5,16 @@ class EuExitPage < BasePage
     @pathway = pathway
   end
 
-  section :content, '#main-content' do
-    element :header, 'h1', text: I18n.t('steps.details.eu_exit.edit.heading')
+  def self.translate_with_appeal_or_application(path)
+    appeal_or_application = @pathway == 'closure' ? 'appeal' : 'application'
+    I18n.t(path,
+      appeal_or_application: I18n.t("generic.appeal_or_application.#{appeal_or_application}")
+    )
   end
+
+  section :content, '#main-content' do
+    element :header, 'h1', text: EuExitPage.translate_with_appeal_or_application(
+          'steps.shared.eu_exit.edit.heading')
+  end
+
 end
