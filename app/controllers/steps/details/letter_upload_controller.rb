04f51ae31efd439e74a7e2687c7e0fc924bd21a2
kalvinlyu <kalvin.lyu@solirius.com>
2022-06-21 11:14:59 +0100

    RST-4458 Push for Will to see code.

--- app/controllers/steps/details/letter_upload_controller.rb
+++ app/controllers/steps/details/letter_upload_controller.rb
@@ -2,6 +2,8 @@ module Steps::Details
   class LetterUploadController < Steps::DetailsStepController
     helper LetterTypeHelper
 
+    before_action :set_documents_list, only: [:edit, :update]
+
     def edit
       @form_object = LetterUploadForm.new(
         tribunal_case: current_tribunal_case,
@@ -12,6 +14,12 @@ module Steps::Details
 
     def update
       update_and_advance(LetterUploadForm, as: :letter_upload)
+    end
+
+    private
+
+    def set_documents_list
+      @document_list = current_tribunal_case&.documents(:supporting_documents) || []
     end
   end
 end
