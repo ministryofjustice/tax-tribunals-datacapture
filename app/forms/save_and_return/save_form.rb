8b108f90ad1a7f1c2925f288faceb98ad31b829e
kalvinlyu <kalvin.lyu@solirius.com>
2022-07-21 15:04:59 +0100

    RST-4571
    Rubocop

--- app/forms/save_and_return/save_form.rb
+++ app/forms/save_and_return/save_form.rb
@@ -3,6 +3,5 @@ module SaveAndReturn
     attribute :save_or_return, String
 
     validates :save_or_return, inclusion: { in: [:save_for_later, :return_to_saved_appeal] }, allow_nil: true
-
   end
 end
