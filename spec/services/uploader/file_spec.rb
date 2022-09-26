8b42ffcd5e52b7efaa8fa47d5cebd915771bead6
Will Taylor <wrftaylor@gmail.com>
2022-09-26 12:31:37 +0100

    fix tests

--- spec/services/uploader/file_spec.rb
+++ spec/services/uploader/file_spec.rb
@@ -4,6 +4,14 @@ RSpec.describe Uploader::File do
 
   let(:filepath) { 'deb757fc-f06d-4233-b260-11edcb7416f3/grounds_for_appeal/letter.jpg' }
 
+  before do
+    allow(Azure::Storage::Blob::BlobService).to receive(:create).and_return(
+      double('client', generate_uri: 'www.example.com'))
+    allow(ENV).to receive(:fetch).with('AZURE_STORAGE_ACCOUNT').and_return('123')
+    allow(ENV).to receive(:fetch).with('AZURE_STORAGE_KEY').and_return('123')
+    allow(ENV).to receive(:fetch).with('AZURE_STORAGE_CONTAINER').and_return('123')
+  end
+
   it 'gets a signed url' do
     expect_any_instance_of(Azure::Storage::Common::Core::Auth::SharedAccessSignature
       ).to receive(:signed_uri).and_return(double('signed_uri', to_s: 'signed_url'))
