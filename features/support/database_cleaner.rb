5fa421fc60767a3125e47f844e648c6e69e7ce90
Avi Singh <avi.singh@solirius.com>
2023-06-14 16:37:08 +0100

    Original database_cleaner.rb file restored

--- features/support/database_cleaner.rb
+++ features/support/database_cleaner.rb
@@ -1,5 +1,5 @@
 DatabaseCleaner.strategy = :truncation
-# DatabaseCleaner.url_allowlist = ['postgresql://postgres@db/tax-tribunals-datacapture']
+DatabaseCleaner.url_allowlist = ['postgresql://postgres@db/tax-tribunals-datacapture']
 
 Before do
   DatabaseCleaner.clean
